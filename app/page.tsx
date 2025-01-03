07b1f88b71e29975b0b197b21234e6bace1fab80
TNDKien <kienn770@gmail.com>
2025-01-03 18:39:22 +0100

    added intro + picture

--- app/page.tsx
+++ app/page.tsx
@@ -14,8 +14,21 @@ const Home: NextPage<Props> = async () => {
   }
 
   return (
-    <div>
+    <div className="container mx-auto max-w-2xl flex flex-col space-y-2">
       <StoryblokStory story={data.story} />
+
+      <h1 className="font-semibold text-2xl">Welcome to my blog!</h1>
+      <p className="leading-7">
+        Hey there! I'm Kien Dang, a second-year student Fontys University of
+        Applied Sciences. I'm currently studying ICT & Media Design, focussing
+        on webdesign and development. At the moment I'm more into front-end
+        developement rather than webdesign.
+        <br />
+        <br />
+        You can find all my articles in the gallery or you can navigate to them
+        in the sidebar, which I organised on category. I started writing in my
+        3rd semester, december 2024. I hope you enjoy reading them!
+      </p>
     </div>
   );
 };
