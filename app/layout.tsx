f009226b33dc8a45dc87d7404677a06b24fd45a3
TNDKien <kienn770@gmail.com>
2025-01-22 01:31:45 +0100

    added Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,31 @@ import "./globals.css";
 import { storyblokInit, apiPlugin } from "@storyblok/react/rsc";
 import StoryblokProvider from "../components/StoryblokProvider";
 import Sidebar from "../components/Sidebar/Sidebar";
+import { Metadata } from "next";
 
 storyblokInit({
   accessToken: process.env.NEXT_PUBLIC_STORYBLOK_ACCESS_TOKEN,
   use: [apiPlugin],
 });
+
+export const metadata: Metadata = {
+  title: {
+    default: "Kien Dang | Web Developer Blog",
+    template: "%s | Kien Dang",
+  },
+  description:
+    "Web development insights, tutorials, and experiences shared by Kien Dang",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://kiendang.nl",
+    siteName: "Kien Dang Blog",
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+};
 
 export default async function RootLayout({
   children,
